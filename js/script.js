6c6f50b03af87338a4eeef483e87830d486d6e0c
rodrigocrn <rodrigocirinocl@gmail.com>
2024-02-24 16:40:15 -0300

    add remove button

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
-const addInput = document.querySelector("#addItem")
-const addBtn = document.querySelector("#addBtn")
-const list = document.querySelector("#list")
+const addInput = document.querySelector("#addItem");
+const addBtn = document.querySelector("#addBtn");
+const list = document.querySelector("#list");
 
 let buttonGlobal;
 let divGlobal;
 
-addBtn.addEventListener("click",(e) => {
-    e.preventDefault()
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    const div = document.createElement("div")
-    const content = document.createElement("p")
-    const removeBtn = document.createElement("button")
+  const div = document.createElement("div");
+  const content = document.createElement("p");
+  const removeBtn = document.createElement("button");
 
-    //add o p com class list item
-    div.classList.add("list-itens")
-    content.textContent = addInput.value
+  //add o p com class list item
+  div.classList.add("list-itens");
+  content.textContent = addInput.value;
 
-    //add o botao com class remove
-    removeBtn.classList.add("removeBtn")
-    removeBtn.innerHTML = "Apagar"
+  //add o botao com class remove
+  removeBtn.classList.add("removeBtn");
+  removeBtn.innerHTML = "Apagar";
 
-    //usando o appendChild
-    div.appendChild(content) 
-    div.appendChild(removeBtn) 
+  //usando o appendChild
+  div.appendChild(content);
+  div.appendChild(removeBtn);
 
-    list.appendChild(div)
+  list.appendChild(div);
 
-    addInput.value = ""
+  addInput.value = "";
 
-    removeBtn.addEventListener("click",() => {
-        list.removeChild(div)
-    })
-})
+  removeBtn.addEventListener("click", () => {
+    list.removeChild(div);
+  });
+});
