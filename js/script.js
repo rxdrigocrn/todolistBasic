9144c981d14949e9c42b2bc18bbaebd101e812ba
rodrigocrn <rodrigocirinocl@gmail.com>
2024-02-24 16:37:49 -0300

    main

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ const addInput = document.querySelector("#addItem")
 const addBtn = document.querySelector("#addBtn")
 const list = document.querySelector("#list")
 
+let buttonGlobal;
+let divGlobal;
+
 addBtn.addEventListener("click",(e) => {
+    e.preventDefault()
+
     const div = document.createElement("div")
     const content = document.createElement("p")
     const removeBtn = document.createElement("button")
@@ -16,13 +21,14 @@ addBtn.addEventListener("click",(e) => {
     removeBtn.innerHTML = "Apagar"
 
     //usando o appendChild
-    div.appendChild(content)
-    div.appendChild(removeBtn)
+    div.appendChild(content) 
+    div.appendChild(removeBtn) 
 
     list.appendChild(div)
+
+    addInput.value = ""
+
+    removeBtn.addEventListener("click",() => {
+        list.removeChild(div)
+    })
 })
-
-
-function clearBtn(button){
-
-}
