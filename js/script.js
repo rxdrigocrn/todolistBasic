d72e0f703c4c935b8eba48076058b9a56d5f7dbe
rodrigocrn <rodrigocirinocl@gmail.com>
2024-02-25 13:18:51 -0300

    add validation

--- js/script.js
+++ js/script.js
@@ -5,30 +5,41 @@ const list = document.querySelector("#list");
 let buttonGlobal;
 let divGlobal;
 
+
+
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const div = document.createElement("div");
-  const content = document.createElement("p");
-  const removeBtn = document.createElement("button");
+  
+  if(addInput.value === ""){
+    alert("Digite uma tarefa válida")
+  } else{
+    const div = document.createElement("div");
+    const content = document.createElement("p");
+    const removeBtn = document.createElement("button");
+  
+    //add o p com class list item
+    div.classList.add("list-itens");
+    content.textContent = addInput.value;
+  
+    //add o botao com class remove
+    removeBtn.classList.add("removeBtn");
+    removeBtn.innerHTML = "Apagar";
+  
+    //usando o appendChild
+    div.appendChild(content);
+    div.appendChild(removeBtn);
+  
+    list.appendChild(div);
+  
+   
+    addInput.value = "";
 
-  //add o p com class list item
-  div.classList.add("list-itens");
-  content.textContent = addInput.value;
+    removeBtn.addEventListener("click", () => {
+      list.removeChild(div);
+    });
+  }
+ 
 
-  //add o botao com class remove
-  removeBtn.classList.add("removeBtn");
-  removeBtn.innerHTML = "Apagar";
 
-  //usando o appendChild
-  div.appendChild(content);
-  div.appendChild(removeBtn);
-
-  list.appendChild(div);
-
-  addInput.value = "";
-
-  removeBtn.addEventListener("click", () => {
-    list.removeChild(div);
-  });
 });
